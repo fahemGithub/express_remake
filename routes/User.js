a65e67bcdce1c0ea4189a5dccd52666d7059049e
fahemGithub <fahemeducaton@gmail.com>
2023-12-28 00:08:34 +0100

    Adding routes

--- routes/User.js
+++ routes/User.js
@@ -1,6 +1,6 @@
 import express from "express";
 const router = express.Router();
-router.use(express.json())
+router.use(express.json());
 let users = [];
 
 // Adding users by id's
@@ -11,20 +11,19 @@ router.post("/new/:id", (req, res) => {
     res.status(200).send(`The user ${name} has been added successfully`);
 });
 
-
 router.get("/", (req, res) => {
     res.send("The users list is: " + users);
 });
 
-let messages = []
+let messages = [];
 
-router.post('/send', (req,res) =>{
-    let msg = req.body.text
-    messages.push(msg)
+router.post("/send", (req, res) => {
+    let msg = req.body.text;
+    messages.push(msg);
     res.status(200).send(`The message has been sent!`);
-})
-router.get('/show',(req,   res)=>       {
-    res.send(`The messages are: ${messages}`)
-})
+});
+router.get("/show", (req, res) => {
+    res.send(`The messages are: ${messages}`);
+});
 
 export default router;
